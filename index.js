963aeb21e4e92bdf3fc030160c2e938056852521
Ehoneah Frank <ehoneahfrank2017@gmail.com>
2024-06-26 16:16:20 +0000

    Express Middleware

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
-import recipeRouter from "./routes/recipes.js";
+import mongoose from "mongoose";
+import recipeRouter from "./routes/recipe.js";
+import 'dotenv/config';
 
+
+
+// Connect to database
+await mongoose.connect(process.env.MONGO_URL)
 // create Express App
 const app = express();
+
+
+//Apply middlewares
+app.use(express.json());
 
 // Define routes
 app.get('/', (req, res)=>{
@@ -27,3 +37,6 @@ app.use(recipeRouter);
 app.listen(3000, () =>{
     console.log('App listening on port 3000')
 });
+
+
+// f3CLaqg3Xu5oXqJu
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import { RecipeModel } from "../models/recipe.js";
 
 // Create router
 const recipeRouter = Router();
@@ -9,8 +9,10 @@ recipeRouter.get('/recipes', (req, res) =>{
     res.json("All recipes");
 });
 
-recipeRouter.post('/recipes', (req, res) =>{
-    res.json("Recipe added");
+recipeRouter.post('/recipes', async (req, res) =>{
+    // Add recipe to database
+    await RecipeModel.create(req.body);
+    res.json('Recipe Added');
 });
 
 recipeRouter.patch('/recipes/:id', (req, res) =>{
@@ -21,5 +23,8 @@ recipeRouter.delete('/recipes/:id', (req, res) =>{
     res.json(`Recipe with ID ${req.params.id} deleted`);
 });
 
+recipeRouter.get('/recipes/:id', (req, res) =>{
+    res.json(`Recipe with ID ${req.params.id} is shown`);
+});
 // Export Router
 export default recipeRouter;
